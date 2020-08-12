9da20910bd911d6e2396f9f94e2929a63a467f2d
apple <apple@appledeMacBook-Air.local>
2020-08-12 11:07:39 +0800

    add subclass

---
+++ Pods/MGCategoryKit/MGCategoryKit/SubClasses/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  MGMainProject
+//
+//  Created by apple on 2020/8/12.
+//  Copyright © 2020 apple. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
